d45fb2eae91519298ca23d88079ca9b8cd7fbb87
Emily Arsenault <erarsena@gmail.com>
2018-03-31 13:08:21 -0500

    wrote the functions

--- UnitTesting_HW/base_sig.R
+++ UnitTesting_HW/base_sig.R
@@ -1,1 +1,16 @@
-#Function to calculate baseline primary producer d15N signature
+#Takes mussel d15N signature data seperated by site and returns a dataframe with two columns (site and mean d15N)
+#
+#Args
+#muss      A dataframe with d15N data for mussels at different sites along a river
+#by        The column populated by site number (default = site)
+#
+#Output - a dataframe with these columns
+#site     Site along river
+#base     Mean d15N for primary producers (baseline signature) at a site
+
+base_sig <- function(dat, by) {
+  
+  #for each site in column by
+    #find the mean d15N from column
+}
+
--- UnitTesting_HW/tp_calc.R
+++ UnitTesting_HW/tp_calc.R
@@ -1,2 +1,17 @@
-#Function to calculate trophic positions for each individual fish
-#relies on base_sig
+#Takes output from base_sig to calculate the trophic position for each fish (row) in dataframe
+#
+#Args
+#dat      A dataframe with d15N data for mussels and fish at different sites along a river
+#frac     Trophic discrimination factor used to calculate trophic position (default = 3.4)
+#
+#Output - a dataframe with these columns
+#site     Site along river
+#species  Fish species (character class)
+#tp       Trophic position
+#
+tp_calc <- function(dat, frac) {
+  #call base_sig
+  
+  #for(each fish (row) in the dataset)
+    #use the output of base_sig to calculate trophic position
+}
