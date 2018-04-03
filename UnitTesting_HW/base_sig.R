ac52716d3585c7ec6681c6198096c91a44e6ac96
Emily Arsenault <erarsena@gmail.com>
2018-04-03 18:47:04 -0500

    fixed up base_sig function and unit test

--- UnitTesting_HW/base_sig.R
+++ UnitTesting_HW/base_sig.R
@@ -1,20 +1,15 @@
-#Takes mussel d15N signature data seperated by site and returns a dataframe with two columns (site and mean d15N)
+#Takes mussel d15N signature data seperated by site 
+#and returns a vector with two values (site name and mean primary producer signature for that site)
 #
 #Args
-#muss      A dataframe with d15N data for mussels at different sites along a river
-#frac      Trophic discrimination factor used to calculate trophic position (default = 3.4)
+#sitename     The name of one site 
+#frac         Trophic discrimination factor used to calculate trophic position (default = 3.4)
 #
-#Output - a dataframe with these columns
-#site     Site along river
-#base     Mean d15N for primary producers (baseline signature) at a site
+#Output
+#base         A single number equal to mean primary producer signature at sitename
 
-base_sig <- function(dat, by) {
-  
-  #read in data
- 
-  #for each mussel (row) in muss
-    #calculate primary producer signature by subtracing frac from d15N value
-      #find the mean d15N by site (by)
-  return(as.data.frame(c(site, base)))
-  
+base_sig <- function(sitename, frac) {
+  frac <- 3.4
+  base <- mean((muss$d15N-frac)[muss$site==sitename])
+  return(base)
 }
