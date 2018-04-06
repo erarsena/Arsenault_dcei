914891390e4877b2267e6c80b26be143f7fb6bf1
Emily Arsenault <erarsena@gmail.com>
2018-04-06 12:22:48 -0500

    fooled around with trying to make a map and accomplished absolutely nothing

--- UnitTesting_HW/base_sig.R
+++ UnitTesting_HW/base_sig.R
@@ -1,21 +1,20 @@
 #Takes mussel d15N signature data separated by site and returns a dataframe with two columns
 #
 #Args
-#dat      The dataset 
+#muss     The dataset 
 #frac     Trophic discrimination factor used to calculate trophic position (default = 3.4, most commonly accepted)
 #
 #Output - a data frame with these
 #base     A single number equal to mean primary producer signature at sitename
 
-dat <- read.csv("muss.csv", header = T)
-base_sig <- function(dat, frac=3.4) {
-  frac <- 3.4
+muss <- read.csv("muss.csv", header = T)
+base_sig <- function(muss, frac=3.4) {
   site <- levels(muss$site)
   base <- vector(mode = "numeric")
   for(i in site) {
     base[i] <- mean(muss$d15N[muss$site==i])
   }
-  df <- data.frame(site, base)
-  colnames(df) <- c("Site", "Baseline")
-  return(df)
+  mussdf <- data.frame(site, base)
+  colnames(mussdf) <- c("Site", "Baseline")
+  return(mussdf)
 }
--- UnitTesting_HW/tp_calc.R
+++ UnitTesting_HW/tp_calc.R
@@ -3,21 +3,51 @@
 #Args
 #fish      A dataframe with d15N data for fish at different sites along a river
 #frac      Trophic discrimination factor used to calculate trophic position (default = 3.4)
+#base_sig(i)
 #
 #Output -  A dataframe with these columns
 #site      Site along river
+#megahabitat
 #species   Fish species (character class)
 #id        ID number for individual fish
 #tp        Trophic position
 #
-tp_calc <- function(fish, frac=3.4) {
-  
-  #read in data
-  #call base_sig
-  
-  #for(each fish (row) in the dataset)
-    #use the output of base_sig to calculate trophic position
-  
-  return(as.data.frame(c(site, species, id, tp)))
-  
+muss <- read.csv("muss.csv", header = T)
+base_sig <- function(muss, frac=3.4) {
+  site <- levels(muss$site)
+  base <- vector(mode = "numeric")
+  for(i in site) {
+    base[i] <- mean(muss$d15N[muss$site==i])
+  }
+  mussdf <- data.frame(site, base)
+  colnames(mussdf) <- c("Site", "Baseline")
+  return(mussdf)
 }
+
+####
+
+#this one is for each fish individual, the next one averages that out for each site
+fish <- read.csv("fish.csv", header = T)
+baseline <- base_sig(muss, 3.4)
+tp_calc <- function(fish, frac=3.4, baseline) {
+  tp <- vector(mode = "numeric")
+  fishy <- fish$id
+  megahabitat <- fish$____
+  for(i in fishy) {
+  tp[i] <- (((fishy-baseline)/3.4)+1)
+}
+  tpdf <- data.frame(site, megahabitat, species, id, tp)
+  colnames(tpdf) <- c("Site", "Megahabitat", "Species", "ID", "Trophic Position")
+  return(tpdf)
+}
+
+
+#For each value in column A, do this formula to it and put it in a new column (X)
+
+trophic <- na.omit(fish$d15N)
+tp <- vector()
+for(i in trophic) {
+  tp <- (i/3.4)+1
+  tp <- m
+}
+#TP = ((fish d15N-mean pp sig)/frac)+1
