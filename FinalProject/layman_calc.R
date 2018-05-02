7e6ca15dd67e54580c84497aba066cf1d9d14ef8
Emily Arsenault <erarsena@gmail.com>
2018-05-01 22:01:02 -0500

    added a new function, layman_calc.R

--- FinalProject/layman_calc.R
+++ FinalProject/layman_calc.R
@@ -1,5 +1,28 @@
-site <- tpdf$site
-for(i in site) {
-  #calculate the range of N values (max-min)
-  #calculate the range of C values (max-min)
+#Generates table with several Layman's metric calculations
+#
+#Args
+#
+##lay (a dataframe with these columns):
+  #"site"
+  #"d13C"
+  #"d15N"
+##group
+#
+##Output (a dataframe with these columns):
+#
+#
+#
+hgmcomplex <- c() #in order of: MMR, PEP, SCR, WIN
+tpdf <- tp_calc(fish, 3.4)
+layman_calc <- function(lay, group) {
+  site <- tpdf$site
+  rangeN <- vector(mode = "numeric")
+  rangeC <- vector(mode = "numeric")
+  for(i in site) {
+    rangeN[i] <- (max(lay$d15N[lay$site==i])-min(lay$d15N[lay$site==i]))
+    rangeC[i] <- (max(lay$d13C[lay$site==i])-min(lay$d13C[lay$site==i]))
+  }
+  laydf <- data.frame(rangeN, rangeC)
+  colnames(laydf) <- c("rangeN", "rangeC")
+  return(laydf)
 }
