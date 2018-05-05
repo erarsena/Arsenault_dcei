17b3fdc26ef8522bc13bdf636ccfef074aea6371
Emily Arsenault <erarsena@gmail.com>
2018-05-05 13:58:54 -0500

    almost done - just needs minor tweaks and text edits

--- FinalProject/base_sig.R
+++ FinalProject/base_sig.R
@@ -3,8 +3,8 @@
 ##Args
 #
 #muss (a data frame with these columns):
-    #"site"     Name of site where primary consumer (mussel) was collected
-    #"d15N"     Primary producer d15N signatures for each individual (row) categorized by "site"
+  #"site"     Name of site where primary consumer (mussel) was collected
+  #"d15N"     Primary producer d15N signatures for each individual (row) categorized by "site"
 #frac  Trophic discrimination factor (default = 3.4)
 #
 ##Output (a data frame with these columns):
@@ -12,12 +12,10 @@
   #"baseline"   A single number equal to mean primary producer d15N signature at "site" (numeric)
 #
 base_sig <- function(muss, frac = 3.4) {
-  site <- muss$site
-  d15N <- muss$d15N
-  sitename <- levels(site)
+  sitename <- levels(muss$site)
   base <- vector(mode = "numeric")
   for(i in sitename) {
-    base[i] <- mean(d15N[site==i]-frac)
+    base[i] <- mean(muss$d15N[muss$site==i]-frac)
   }
   mussdf <- data.frame(sitename, base)
   colnames(mussdf) <- c("site", "baseline")
--- FinalProject/layman_calc.R
+++ FinalProject/layman_calc.R
@@ -1,24 +1,24 @@
-#Generates table with six Layman's metric calculations
+#Generates list with six Layman's metric calculations separated by site
 #
 ##Args
 #
-#tpdf (a dataframe with these columns):
-  #"site"
-  #"d13C"
-  #"d15N"
-##Output (a dataframe with these rows, columns designated by argument 'group'):
-  #"dY_range"
-  #"dX_range"
-  #"TA"
-  #"CD"
-  #"NND"
-  #"SDNDD"
+#dat (a dataframe with these columns):
+  #"site"       Name of site where consumer (fish) was collected
+  #"d13C"       Consumer d13C signature data
+  #"d15N"       Consumer d15N signature data
+##Output (a list with these metrics, listed by argument 'site'):
+  #"dY_range"   range of all d15N values (max-min)
+  #"dX_range"   range of all d13C values (max-min)
+  #"TA"         total area of the convex hull
+  #"CD"         mean distance to centroid
+  #"NND"        mean nearest neighbor distance
+  #"SDNDD"      standard deviation of the nearest neighbor distance
 #
 layman_calc <- function(dat) {
   require("SIBER")
   lay <- vector(mode = "numeric")
   for(i in levels(dat$site)) {
-  lay[i] <- SIBER::laymanMetrics(dat$d13C[dat$site==i], dat$d15N[dat$site==i])
+  lay[i] <- list(SIBER::laymanMetrics(dat$d13C[dat$site==i], dat$d15N[dat$site==i]))
   }
-  return(as.data.frame(lay))
+  return(lay)
 }
